ab41528111391e7db8dd76ee4899abe26ea1febf
Silvana Saavedra Londoño <silvana.saavedra@udea.edu.co>
2026-09-12 17:35:27 -0500

    Update PricingRuleRequest

--- Aerodynamic/Backend/aerodynamic.pricing-engine/aerodynamic.pricing-engine/src/main/java/com/aerodynamic/aerodynamic/pricing_engine/dto/PricingRuleRequest.java
+++ Aerodynamic/Backend/aerodynamic.pricing-engine/aerodynamic.pricing-engine/src/main/java/com/aerodynamic/aerodynamic/pricing_engine/dto/PricingRuleRequest.java
@@ -1,106 +1,98 @@
 package com.aerodynamic.aerodynamic.pricing_engine.dto;
 
 import com.aerodynamic.aerodynamic.pricing_engine.model.enums.BussinessVariable;
-import com.aerodynamic.aerodynamic.pricing_engine.model.enums.RuleCondition;
+import com.aerodynamic.aerodynamic.pricing_engine.model.enums.ConditionOperator;
 import com.aerodynamic.aerodynamic.pricing_engine.model.enums.AdjustmentType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+public class PricingRuleRequest {
 
-// DTO used to receive the information needed to create or update a pricing rule.
-public class PricingRuleRequest { 
+    // Name of the pricing rule.
+    @NotBlank(message = "Rule name is required")
+    private String ruleName;
 
-    // Rule name is required and cannot be null or empty.
-    @NotBlank(message = "Rule name is required") 
-    private String ruleName; 
+    // Business variable used by the pricing rule.
+    @NotNull(message = "Business variable is required")
+    private BussinessVariable bussinessVariable;
 
-    // Business variable to which the rule will be applied.
-    // This field is required.
-    @NotNull(message = "Business variable is required") 
-    private BussinessVariable bussinessVariable; 
+    // Operator used to evaluate the condition.
+    @NotNull(message = "Condition operator is required")
+    private ConditionOperator conditionOperator;
 
-    // Condition that determines when the pricing rule should be applied.
-    // This field is required.
-    @NotNull(message = "Condition is required") 
-    private RuleCondition condition; 
+    // Value used in the condition.
+    @NotNull(message = "Condition value is required")
+    private Double conditionValue;
 
-    // Value used to evaluate the condition.
-    // This field is required.
-    @NotNull(message = "Condition value is required") 
-    private Double conditionOperator; 
-
-    // Defines how the price adjustment will be applied.
-    // This field is required.
-    @NotNull(message = "Adjustment type is required") 
-    private AdjustmentType adjustmentType; 
+    // Type of adjustment that will be applied to the price.
+    @NotNull(message = "Adjustment type is required")
+    private AdjustmentType adjustmentType;
 
     // Value of the price adjustment.
-    // This field is required.
-    @NotNull(message = "Adjustment value is required") 
-    private Double adjustmentValue; 
+    @NotNull(message = "Adjustment value is required")
+    private Double adjustmentValue;
 
-    // Empty constructor required to create an instance of this DTO.
-    public PricingRuleRequest() { 
-    } 
+    // Empty constructor.
+    public PricingRuleRequest() {
+    }
 
-    // Returns the name of the pricing rule.
-    public String getRuleName() { 
-        return ruleName; 
-    } 
+    // Returns the rule name.
+    public String getRuleName() {
+        return ruleName;
+    }
 
-    // Sets the name of the pricing rule.
-    public void setRuleName(String ruleName) { 
-        this.ruleName = ruleName; 
-    } 
+    // Sets the rule name.
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
 
-    // Returns the business variable associated with the rule.
-    public BussinessVariable getBussinessVariable() { 
-        return bussinessVariable; 
-    } 
+    // Returns the business variable.
+    public BussinessVariable getBussinessVariable() {
+        return bussinessVariable;
+    }
 
-    // Sets the business variable associated with the rule.
-    public void setBussinessVariable(BussinessVariable bussinessVariable) { 
-        this.bussinessVariable = bussinessVariable; 
-    } 
+    // Sets the business variable.
+    public void setBussinessVariable(BussinessVariable bussinessVariable) {
+        this.bussinessVariable = bussinessVariable;
+    }
 
-    // Returns the condition of the pricing rule.
-    public RuleCondition getCondition() { 
-        return condition; 
-    } 
+    // Returns the condition operator.
+    public ConditionOperator getConditionOperator() {
+        return conditionOperator;
+    }
 
-    // Sets the condition of the pricing rule.
-    public void setCondition(RuleCondition condition) { 
-        this.condition = condition; 
-    } 
+    // Sets the condition operator.
+    public void setConditionOperator(ConditionOperator conditionOperator) {
+        this.conditionOperator = conditionOperator;
+    }
 
-    // Returns the value used to evaluate the condition.
-    public Double getConditionOperator() { 
-        return conditionOperator; 
-    } 
+    // Returns the condition value.
+    public Double getConditionValue() {
+        return conditionValue;
+    }
 
-    // Sets the value used to evaluate the condition.
-    public void setConditionOperator(Double conditionValue) { 
-        this.conditionOperator = conditionValue; 
-    } 
+    // Sets the condition value.
+    public void setConditionValue(Double conditionValue) {
+        this.conditionValue = conditionValue;
+    }
 
     // Returns the adjustment type.
-    public AdjustmentType getAdjustmentType() { 
-        return adjustmentType; 
-    } 
+    public AdjustmentType getAdjustmentType() {
+        return adjustmentType;
+    }
 
     // Sets the adjustment type.
-    public void setAdjustmentType(AdjustmentType adjustmentType) { 
-        this.adjustmentType = adjustmentType; 
-    } 
+    public void setAdjustmentType(AdjustmentType adjustmentType) {
+        this.adjustmentType = adjustmentType;
+    }
 
     // Returns the adjustment value.
-    public Double getAdjustmentValue() { 
-        return adjustmentValue; 
-    } 
+    public Double getAdjustmentValue() {
+        return adjustmentValue;
+    }
 
     // Sets the adjustment value.
-    public void setAdjustmentValue(Double adjustmentValue) { 
-        this.adjustmentValue = adjustmentValue; 
-    } 
+    public void setAdjustmentValue(Double adjustmentValue) {
+        this.adjustmentValue = adjustmentValue;
+    }
 }
-
